87d7b2319a0a84b80075987147d4431d5200be0d
Valeria C S Morais <valeriacsmorais@gmail.com>
2021-03-06 17:07:06 -0300

    inclusao de 2 condicoes para emprestimo imobiliario

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/Solucao.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/Solucao.java
@@ -2,6 +2,7 @@ package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique;
 
 import entity.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,11 +15,22 @@ public class Solucao {
 
     public static String processMessages(List<String> messages) {
       List<Proposal> proposalList = new ArrayList<>();
+      List<Proposal> checkProposalList = new ArrayList<>();
 
       for (String message: messages) {
           run(proposalList, message);
       };
 
-	  return proposalList.stream().map(proposal -> proposal.getId().toString()).collect(Collectors.joining(DELIMITER));
+      boolean success = false;
+
+      for(Proposal proposal: proposalList){
+          success = proposal.getProposalLoanValue().compareTo(BigDecimal.valueOf(30000)) > 0 &&
+                  proposal.getProposalLoanValue().compareTo(BigDecimal.valueOf(30000)) < 0 ? true : false;
+          success = proposal.getProposalNumberOfMonthlyInstallments()>=24 && proposal.getProposalNumberOfMonthlyInstallments()<=180 ? true: false;
+          if (success) checkProposalList.add(proposal);
+          success = false;
+      }
+
+	  return checkProposalList.stream().map(proposal -> proposal.getId().toString()).collect(Collectors.joining(DELIMITER));
   }
 }
